71381adc0714cbe2f7908d543f032df98f8d0a4b
griminir <viktor.elliot.king@gmail.com>
2024-05-30 23:44:35 +0200

    Add project files.

---
+++ ExtensionMethodsHomwork/ConsoleHelper.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsHomwork
+{
+    public static class ConsoleHelper
+    {
+        public static string RequestString(this string message)
+        {
+            string output = "";
+
+
+            while (string.IsNullOrWhiteSpace(output))
+            {
+                Console.WriteLine(message);
+                output = Console.ReadLine();
+            }
+
+            return output;
+        }
+    }
+}
---
+++ ExtensionMethodsHomwork/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethodsHomwork
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new PersonModel();
+            person.FirstName = "What is your first name: ".RequestString();
+
+            Console.ReadLine();
+        }
+    }
+}
